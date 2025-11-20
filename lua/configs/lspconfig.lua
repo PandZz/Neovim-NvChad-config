d70e4ed499d9de945a95493f128ca24ed76fa704
PandZz <2739468442@qq.com>
2025-11-20 20:46:59 +0800

    Update lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "lua_ls", "pyright" }
+local servers = { 
+  "html",
+  "cssls",
+  "clangd",
+  "lua_ls",
+  "pyright",
+  "rust-analyzer"
+}
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
